6a1acb31fc914f51c2da5e5371ffcf6f9d516716
Cliff Senkbeil <cliff@netactuary.com.au>
2015-09-25 13:56:16 +1000

    add sample haskell functions

---
+++ recursiveFib.hs
@@ -0,0 +1,5 @@
+fib :: Integer -> Integer
+fib n
+    | n == 0  = 0
+    | n == 1  = 1
+    | n > 1   = fib (n-1) + fib (n-2)
---
+++ tailRecursiveFib.hs
@@ -0,0 +1,4 @@
+fibs :: [Integer]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+fastfib n = fibs !! n
